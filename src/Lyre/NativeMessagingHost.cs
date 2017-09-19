5b7d8081ecd85794bf9f509f8fc7c17ee1289a31
Alex Wiese <alex@wiese.com.au>
2017-09-19 10:06:59 +0930

    Added ReadAsString method and some optimizations.

--- src/Lyre/NativeMessagingHost.cs
+++ src/Lyre/NativeMessagingHost.cs
@@ -12,10 +12,10 @@ namespace Lyre
         private readonly Stream _inStream;
         private readonly Stream _outStream;
         private readonly Encoding _encoding;
-        private readonly Decoder _decoder;
         private readonly JsonSerializerSettings _jsonSerializerSettings;
 
-        private readonly byte[] _buffer = new byte[sizeof(int)];
+        private readonly byte[] _sendLengthBuffer = new byte[sizeof(int)];
+        private readonly byte[] _receiveLengthBuffer = new byte[sizeof(int)];
 
         private static readonly JsonSerializerSettings DefaultSerializerSettings = new JsonSerializerSettings
         {
@@ -34,31 +34,54 @@ namespace Lyre
             _outStream = outStream ?? throw new ArgumentNullException(nameof(outStream));
             _encoding = encoding ?? throw new ArgumentNullException(nameof(encoding));
             _jsonSerializerSettings = jsonSerializerSettings ?? throw new ArgumentNullException(nameof(jsonSerializerSettings));
-
-            _decoder = encoding.GetDecoder();
         }
-        
+
         public async Task Write(object value)
         {
             var jsonString = JsonConvert.SerializeObject(value, _jsonSerializerSettings);
 
             // Use the encoding to get the length of the 
-            // buffer, may not be the number of chars
+            // buffer, it may not be the number of chars
             var byteBuffer = _encoding.GetBytes(jsonString);
-            var lengthBuffer = BitConverter.GetBytes(byteBuffer.Length);
+            var byteBufferLength = byteBuffer.Length;
 
-            // Send buffer length (Int32) then the JSON
-            await _outStream.WriteAsync(lengthBuffer, 0, lengthBuffer.Length);
-            await _outStream.WriteAsync(byteBuffer, 0, byteBuffer.Length);
+            _sendLengthBuffer[0] = (byte)byteBufferLength;
+            _sendLengthBuffer[1] = (byte)(byteBufferLength >> 8);
+            _sendLengthBuffer[2] = (byte)(byteBufferLength >> 16);
+            _sendLengthBuffer[3] = (byte)(byteBufferLength >> 24);
+
+            // Send the buffer length (Int32) then the JSON
+            await _outStream.WriteAsync(_sendLengthBuffer, 0, sizeof(int));
+            await _outStream.WriteAsync(byteBuffer, 0, byteBufferLength);
         }
-        
+
+        /// <summary>
+        /// Reads and deserializes a JSON message from the Native Messaging input stream.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <returns>The deserialized object from the JSON string.</returns>
         public async Task<T> Read<T>()
+        {
+            var jsonString = await ReadAsString();
+
+            return JsonConvert.DeserializeObject<T>(jsonString, _jsonSerializerSettings);
+        }
+
+        /// <summary>
+        /// Reads a JSON string from the Native Messaging input stream.
+        /// </summary>
+        public async Task<string> ReadAsString()
         {
             var length = await ReadInt32();
 
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), length, "Length of message cannot be negative");
+            }
+
             var jsonString = await ReadString(length);
-            
-            return JsonConvert.DeserializeObject<T>(jsonString, _jsonSerializerSettings);
+
+            return jsonString;
         }
 
         private async Task<int> ReadInt32()
@@ -67,7 +90,7 @@ namespace Lyre
 
             do
             {
-                var read = await _inStream.ReadAsync(_buffer, 0, sizeof(int));
+                var read = await _inStream.ReadAsync(_receiveLengthBuffer, 0, sizeof(int) - bytesRead);
 
                 if (read == 0)
                 {
@@ -77,7 +100,7 @@ namespace Lyre
                 bytesRead += read;
             } while (bytesRead < sizeof(int));
 
-            return _buffer[0] | _buffer[1] << 8 | _buffer[2] << 16 | _buffer[3] << 24;
+            return _receiveLengthBuffer[0] | _receiveLengthBuffer[1] << 8 | _receiveLengthBuffer[2] << 16 | _receiveLengthBuffer[3] << 24;
         }
 
         private async Task<string> ReadString(int numberOfBytes)
@@ -87,20 +110,16 @@ namespace Lyre
                 return string.Empty;
             }
 
+            // We know the length of the expected message
             var byteBuffer = new byte[numberOfBytes];
 
-            // The number of characters depends on the encoding
-            // The char buffer length is defined as the 
-            // maximum number of chars that can be represented in 
-            // this many bytes for the Encoding used
-            var charBuffer = new char[_encoding.GetMaxCharCount(numberOfBytes)];
-
             var bytesRead = 0;
-            var charactersRead = 0;
 
+            // This will read from the stream into the byteBuffer 
+            // until all bytes have been received
             do
             {
-                var read = await _inStream.ReadAsync(byteBuffer, 0, numberOfBytes - bytesRead);
+                var read = await _inStream.ReadAsync(byteBuffer, bytesRead, numberOfBytes - bytesRead);
 
                 if (read == 0)
                 {
@@ -108,12 +127,11 @@ namespace Lyre
                     throw new EndOfStreamException();
                 }
 
-                charactersRead += _decoder.GetChars(byteBuffer, 0, read, charBuffer, charactersRead);
                 bytesRead += read;
 
             } while (bytesRead < numberOfBytes);
 
-            return new string(charBuffer, 0, charactersRead);
+            return _encoding.GetString(byteBuffer);
         }
     }
 }
